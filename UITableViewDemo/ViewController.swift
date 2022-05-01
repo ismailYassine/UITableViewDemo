27d6b87618c7eb5dc6c891ce8f5926637084edb4
ismail <yassine.iguire@gmail.com>
2022-04-30 21:14:19 -0400

    Initial Commit

---
+++ UITableViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewDemo
+//
+//  Created by Ismail on 2022-04-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

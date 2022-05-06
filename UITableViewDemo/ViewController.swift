915ea398053f05554d39da3bbd6d5b7c99f726e1
Yassine Ismail <yassine.iguire@gmail.com>
2022-05-06 08:58:54 -0400

    UITableView implemented

--- UITableViewDemo/ViewController.swift
+++ UITableViewDemo/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let countries = [
+        "Canada",
+        "Senegal",
+        "Island",
+        "Japan",
+        "China",
+        "Rwanda"
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
+
 
